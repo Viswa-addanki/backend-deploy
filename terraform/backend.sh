f13d44dcdd19849e517a82e90fbec3b83f19b1b3
Viswa_addanki <a.viswanath007@gmail.com>
2024-09-19 18:49:16 +0530

    some-msg

--- terraform/backend.sh
+++ terraform/backend.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 component=$1
 environment=$2
+app_Version=$3
 dnf install ansible -y
 pip3.9 install botocore boto3
-ansible-pull -i localhost, -U https://github.com/Viswa-addanki/expense-ansible-roles-tf.git main.yaml -e component=$component -e env=$environment
+ansible-pull -i localhost, -U https://github.com/Viswa-addanki/expense-ansible-roles-tf.git main.yaml -e component=$component -e env=$environment -e appVersion=$app_Version
 
 
